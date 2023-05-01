cfa96cfa024238725bb521610725309197e07de4
decl1 <77211193+decl1@users.noreply.github.com>
2023-05-02 00:14:19 +0300

    Базовая система классов

---
+++ Chess/Chess/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+
+class Figure {
+	int f_type; // -1 (none) - 0 (white) - 1 (black)
+};
+
+class King : public Figure {};   // Король
+class Queen : public Figure {};  // Ферзь
+class Rook : public Figure {};   // Ладья
+class Bishop : public Figure {}; // Слон
+class Knight : public Figure {}; // Конь
+class Pawns : public Figure {};  // Пешка
+
+class Deck {
+	Figure figure[8][8];
+};
