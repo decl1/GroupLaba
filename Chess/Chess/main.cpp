24604932a29db6cba4c6031f4a742c73b7d8fcd0
decl1 <77211193+decl1@users.noreply.github.com>
2023-08-27 17:41:51 +0300

    123

--- Chess/Chess/main.cpp
+++ Chess/Chess/main.cpp
@@ -1,19 +1,15 @@
 #include <iostream>
-
-
-class Figure {
-	int f_type; // -1 (none) - 0 (white) - 1 (black)
-};
-
-class King : public Figure {};   // Король
-class Queen : public Figure {};  // Ферзь
-class Rook : public Figure {};   // Ладья
-class Bishop : public Figure {}; // Слон
-class Knight : public Figure {}; // Конь
-class Pawns : public Figure {};  // Пешка
+#include <Windows.h>
 
 class Deck {
-	Figure figure[8][8];
+protected:
+	char DeckArr[64];
+public:
+	void PrintDeck() {
+
+	}
 };
 
-//Тут бы Я
+int main() {
+	
+}
