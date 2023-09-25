8159531f9bcd0992d12a4a134b4cf76743318e1a
decl1 <77211193+decl1@users.noreply.github.com>
2023-09-25 22:35:57 +0300

    bassed classes

--- Shahki/Shahki/main.cpp
+++ Shahki/Shahki/main.cpp
@@ -3,12 +3,25 @@
 // eto shahki
 // pupupu
 
+class shahka {
+protected:
+	int type; // -1 black, 0 neutral, 1 white
+	bool queen; // 0 neutal, 1 - queen
+public:
+	int gettype() {};
+	bool getqueen() {};
+};
 class Deck {
 protected:
-
+	shahka deck[8][8]; 
 public:
 	void printDeck();
+	int hodshashi(int start, int end, const class shahka& sh);
 };
+
+int Deck::hodshashi(int start, int end, const class shahka& sh) {
+
+}
 
 void Deck::printDeck() {
 
