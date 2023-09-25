166848edff078559a164d07abad360f5c93bffa6
decl1 <77211193+decl1@users.noreply.github.com>
2023-09-25 23:19:36 +0300

    deck coords counter

--- Shahki/Shahki/main.cpp
+++ Shahki/Shahki/main.cpp
@@ -1,8 +1,5 @@
 #include <iostream>
 
-// eto shahki
-// pupupu
-
 class shahka {
 protected:
 	int type; // -1 black, 0 neutral, 1 white
@@ -20,14 +17,27 @@ public:
 };
 
 int Deck::hodshashi(int start, int end, const class shahka& sh) {
-
+	return 0;
 }
 
 void Deck::printDeck() {
 
 }
 
+int deckcoordcounter(char x, int y) {
+	if (y >= 1 && y <= 8) {
+		if (x >= 65 && x <= 72)
+			return ((x - 4) % 10 * y) - 1;
+		if (x >= 97 && x <= 104)
+			return ((x - 6) % 10 * y) - 1;
+	}
+	else {
+		return -1;
+	}
+}
 
 void main() {
+	//debug zone
 
+	//debug zone
 }
