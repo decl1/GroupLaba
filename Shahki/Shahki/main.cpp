876d4fc7b0420db6ad8901770be6fc6a046879e0
decl1 <77211193+decl1@users.noreply.github.com>
2023-10-01 19:30:40 +0300

    complete

--- Shahki/Shahki/main.cpp
+++ Shahki/Shahki/main.cpp
@@ -1,18 +1,19 @@
 #include "ClassesAndFunc.h"
 
 int main() {
-	//debug zone
+	//console
+	system("title Shahochki");
+	system("mode con cols=60 lines=40");
+	//console
 
-	//debug zone
-
-	Deck gdeck(1);
+	Deck gdeck; // debug - gdeck(1)
 	bool player_queue = 0; // 0 - white, 1 - black;
 	char xs;
 	int ys;
 	char xe;
 	int ye;
 	int move;
-	while (gdeck.getblacks() != 0 || gdeck.getwhites() != 0) {
+	while (gdeck.getblacks() != 0 && gdeck.getwhites() != 0) {
 		gdeck.printDeck();
 		std::cout << "Queue: " << ((player_queue == 0) ? "W " : "B ") 
 			<< "______ W - " << (12 - gdeck.getblacks()) <<":"<< (12 - gdeck.getwhites()) << " - B " << std::endl;
@@ -33,7 +34,17 @@ int main() {
 			std::cout << "Correct move";
 		}
 		std::cout << std::endl;
-		//system("pause");
 		system("cls");
+
+		if (gdeck.getblacks() == 0) {
+			gdeck.printDeck();
+			std::cout << " WHITE WINS!!! " << "W - " << (12 - gdeck.getblacks()) << ":" << (12 - gdeck.getwhites()) << " - B " << std::endl;;
+			system("pause");
+		}
+		else if (gdeck.getwhites() == 0) {
+			gdeck.printDeck();
+			std::cout << " BLACK WINS!!! " << "W - " << (12 - gdeck.getblacks()) << ":" << (12 - gdeck.getwhites()) << " - B " << std::endl;;
+			system("pause");
+		}
 	}
 }
